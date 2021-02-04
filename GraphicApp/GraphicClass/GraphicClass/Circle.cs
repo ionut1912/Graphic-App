7b0286e79c967af88ca0a78d710228712e68adde
Nitescu Ionut <53054822+ionut1912@users.noreply.github.com>
2021-02-04 15:36:14 +0200

    Add files via upload

---
+++ GraphicApp/GraphicClass/GraphicClass/Circle.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Configuration;
+using System.IO;
+namespace GraphicClass
+{
+    public class Circle : Shape
+    {  public int Radius { get; set; }
+        public Circle()
+        {
+
+        }
+        public Circle(int Radius,Point origin)
+        {
+            this.Radius = Radius;
+            _origin = origin;
+
+        }
+        public override double Area()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+
+        public override void Draw(Graphics graphic, Pen p)
+        {
+            graphic.DrawEllipse(p, _origin.X - Radius, _origin.Y - Radius, Radius + Radius, Radius + Radius);   
+        }
+
+        public override void MoveTo(Point point,Graphics graphics)
+        {
+            _origin = point;
+            graphics.DrawEllipse(p, _origin.X - Radius, _origin.Y - Radius, Radius + Radius, Radius + Radius);
+        }
+
+        public override void Resize(int Size, Graphics graphicContent)
+        {
+            Radius += Size;
+        }
+        public void Save()
+        {
+            string circleAsJson = JsonConvert.SerializeObject(this);
+            string path = $"{ConfigurationManager.AppSettings["CirclePath"]} {this.Id}.json";
+            File.WriteAllText(path, circleAsJson);
+        }
+        public void Load(List<Circle> circles)
+        {
+            string path = ConfigurationManager.AppSettings["CirclePath"];
+            List<string> files = Directory.GetFiles(path).ToList();
+
+            foreach (string file in files)
+            {
+                circles.Add(JsonConvert.DeserializeObject<Circle>(File.ReadAllText(file)));
+            }
+
+        }
+    }
+}
---
+++ GraphicApp/GraphicClass/GraphicClass/GraphicTool.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Configuration;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace GraphicClass
+{
+    public class GraphicTool
+    {
+        public Graphics graphicContent;
+        public Pen p;
+        public List<Shape> _shapeList { get; set; }
+        public GraphicTool(Graphics graphicContent, Pen p)
+        {
+            this._shapeList = new List<Shape>()
+            {
+               new Rectanglee(),new Circle(),new Square()
+            };
+            this.graphicContent = graphicContent;
+            this.p = p;
+        }
+
+        public void DrawAll()
+        {
+
+            foreach (Shape shape in _shapeList)
+            {
+                shape.Draw(graphicContent, p);
+            }
+        }
+
+        public void Load()
+        {
+            string pathSquare = ConfigurationManager.AppSettings["SquarePath"];
+            string pathCircle = ConfigurationManager.AppSettings["CirclePath"];
+            string pathRectangle = ConfigurationManager.AppSettings["RectanglePath"];
+
+            List<string> squares = Directory.GetFiles(pathSquare).ToList();
+
+            foreach (string file in squares)
+            {
+                _shapeList.Add(JsonConvert.DeserializeObject<Square>(File.ReadAllText(file)));
+            }
+
+            List<string> circles = Directory.GetFiles(pathCircle).ToList();
+
+            foreach (string file in circles)
+            {
+                _shapeList.Add(JsonConvert.DeserializeObject<Circle>(File.ReadAllText(file)));
+            }
+
+            List<string> rectangles = Directory.GetFiles(pathRectangle).ToList();
+
+            foreach (string file in rectangles)
+            {
+                _shapeList.Add(JsonConvert.DeserializeObject<Rectanglee>(File.ReadAllText(file)));
+            }
+
+        }
+    }
+}
